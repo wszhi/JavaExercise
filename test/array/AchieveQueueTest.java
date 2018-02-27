48d4a91580cdff5e9ccf32815e49a362ec30a36e
Shengzhi Wang <594321736@qq.com>
2018-02-27 14:31:00 +0800

    [题目3]用两个栈来实现一个队列，完成队列的Push和Pop操作

---
+++ test/array/AchieveQueueTest.java
@@ -0,0 +1,29 @@
+package array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AchieveQueueTest {
+    @Test
+    public void should_return_1_when_pop_once_given_push_1_2_3() {
+        AchieveQueue achieveQueue = new AchieveQueue();
+        achieveQueue.push(1);
+        achieveQueue.push(2);
+        achieveQueue.push(3);
+        assertEquals(1,achieveQueue.pop());
+    }
+
+    @Test
+    public void should_return_3_when_push_1_2_3_pop_twice_push_4_5_pop_once() {
+        AchieveQueue achieveQueue = new AchieveQueue();
+        achieveQueue.push(1);
+        achieveQueue.push(2);
+        achieveQueue.push(3);
+        assertEquals(1,achieveQueue.pop());
+        assertEquals(2,achieveQueue.pop());
+        achieveQueue.push(4);
+        achieveQueue.push(5);
+        assertEquals(3,achieveQueue.pop());
+    }
+}
