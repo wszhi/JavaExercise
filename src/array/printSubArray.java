1eac8b036b8f0acb271580e27a5487e821395200
Shengzhi Wang <594321736@qq.com>
2018-02-27 11:31:42 +0800

    [题目1]打印某个数组的所有子数组

---
+++ src/array/printSubArray.java
@@ -0,0 +1,42 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/*
+题目：
+给出一个数组例如：[10,20,30]
+请写出一个程序，打印数组的所有子数组，例如：[10],[20],[30],[10,20],[10,30],[10,20,30]
+思路：
+采用递归，把当前的数组加到结果list中，然后remove当前元素，对于剩下元素所组成的数组重复以上操作，对于最后的结果list去重即可。
+* */
+public class printSubArray {
+    private void getAllSubArray(List<Integer> originList, List result) {
+        if (originList.size() > 0) {
+            result.add(originList);
+        }
+        for (int i = 0; i < originList.size(); i++) {
+            List<Integer> temp = new ArrayList<>();
+            temp.addAll(originList);
+            temp.remove(new Integer(temp.get(i)));
+            getAllSubArray(temp, result);
+        }
+    }
+
+    private static List removeDuplicate(List list) {
+        HashSet set = new HashSet(list);
+        ArrayList newList = new ArrayList();
+        newList.addAll(set);
+        return newList;
+    }
+
+    public List printAllSubArray(Integer[] originArray) {
+        List<Integer> originList = Arrays.asList(originArray);
+        List<List> result = new ArrayList<>();
+        this.getAllSubArray(originList, result);
+        return this.removeDuplicate(result);
+    }
+
+}
---
+++ test/array/printSubArrayTest.java
@@ -0,0 +1,26 @@
+package array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class printSubArrayTest {
+    @Test
+    public void name() throws Exception {
+        printSubArray printSubArray = new printSubArray();
+        Integer[] testArray = {10, 20, 30};
+        List list = printSubArray.printAllSubArray(testArray);
+        assertEquals(7, list.size());
+        assertTrue(list.contains(Arrays.asList(10)));
+        assertTrue(list.contains(Arrays.asList(20)));
+        assertTrue(list.contains(Arrays.asList(30)));
+        assertTrue(list.contains(Arrays.asList(10, 20)));
+        assertTrue(list.contains(Arrays.asList(10, 30)));
+        assertTrue(list.contains(Arrays.asList(20, 30)));
+        assertTrue(list.contains(Arrays.asList(10, 20, 30)));
+    }
+}
